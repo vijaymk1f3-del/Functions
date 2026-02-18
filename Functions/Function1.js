27deda1d599c2282945a8278858da3cca064f215
vijaymk1f3-del <vijaymk1f3@gmail.com>
2026-02-18 13:45:34 +0530

    chore: I updated Function1.js

---
+++ Functions/Function1.js
@@ -0,0 +1,10 @@
+function nice(name) {
+    console.log("hey "+ name + " you are nice!");
+    console.log("hey "+ name + " you are good!");
+    console.log("hey "+ name + " your tshirt is nice!");
+    console.log("hey "+ name + " your course is good too!");
+}
+
+nice("sara");
+nice("mohammad");
+nice("sara");
